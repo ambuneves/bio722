ea290d0976de7e7a5a5b5f28e8908abb5f1c4c65
Amanda Neves <60668546+ambuneves@users.noreply.github.com>
2021-02-28 11:02:08 -0500

    Updated to include staging and post hoc

--- bio722_group-project_data-analysis.R
+++ bio722_group-project_data-analysis.R
@@ -8,10 +8,12 @@ BiocManager::install("tximport")
 BiocManager::install("DRIMSeq")
 BiocManager::install("DEXSeq")
 BiocManager::install("DESeq2")
+BiocManager::install("stageR")
 library(DEXSeq)
 library(DRIMSeq)
 library(tximport)
 libary(DESeq2)
+library(stageR)
 
 #Load in the sample information collected by Tyler, lists the sample name + condition
 
@@ -77,19 +79,7 @@ d <- dmDSdata(counts=counts, samples=sample_info)
 methods(class=class(d))
 counts(d[1,])[,1:4]
 
-# Tabulating the number of times we see a gene ID
-# then, tabulating the output again
 
-table(table(counts(d)$gene_id))
-
-#Create the design matrix
-design_full <- model.matrix(~condition, data=DRIMSeq::samples(d))
-colnames(design_full)
-
-# Estimate model parameters and check for DTU
-# first estimate the precision
-# Next, fit regression coefficients and perform
-# null hypothesis testing on the coefficient of interest
 
 # Filtering out:
 # n = total sample number
@@ -107,8 +97,117 @@ d <- dmFilter(d,
               min_samps_gene_expr=n, min_gene_expr=10)
 d
 
-# Extracting results table
-dxr <- DEXSeqResults(dxd, independentFiltering=FALSE)
-qval <- perGeneQValue(dxr)
-dxr.g <- data.frame(gene=names(qval),qval)
-dxr.g
+
+# Tabulating the number of times we see a gene ID
+# then, tabulating the output again
+
+table(table(counts(d)$gene_id))
+
+
+#Create the design matrix
+design_full <- model.matrix(~condition, data=DRIMSeq::samples(d))
+colnames(design_full)
+
+# Estimate model parameters and check for DTU
+# first estimate the precision
+# Next, fit regression coefficients and perform
+# null hypothesis testing on the coefficient of interest
+
+set.seed(1)
+system.time({
+  d <- dmPrecision(d, design=design_full)
+  d <- dmFit(d, design=design_full)
+  d <- dmTest(d, coef=c("condition2", "condition3"))
+})
+
+# Check results by making a results table
+
+# Per gene
+res <- DRIMSeq::results(d)
+head(res)
+
+# Per transcript
+res.txp <- DRIMSeq::results(d, level="feature")
+head(res.txp)
+
+# Removing NAs because they cause issues downstream
+
+no.na <- function(x) ifelse(is.na(x), 1, x)
+res$pvalue <- no.na(res$pvalue)
+res.txp$pvalue <- no.na(res.txp$pvalue)
+
+# Plotting proportion for one of the sig. genes
+
+idx <- which(res$adj_pvalue < 0.05)[1]
+res[idx,]
+plotProportions(d, res$gene_id[idx], "condition")
+
+#Messing around with some more plotting
+
+idx.txp <- which(res.txp$adj_pvalue < 0.05)[1]
+res.txp[idx.txp,]
+plotProportions(d, res$gene_id[idx], "condition")
+
+#### Stage R ####
+
+# Loading the full data, rather than the subset
+nrow(res)
+nrow(res.txp)
+
+# Start by asking "which genes contain any evidence of DTU"
+# Do this via 'screening' stage
+# Next ask "which txs in gene may be participating in DTU"
+# Do this via 'confirmation' stage
+
+# Start by constructing a vector of p-values (genes)
+pScreen <- res$pvalue
+strp <- function(x) substr(x,1,15)
+names(pScreen) <- strp(res$gene_id)
+head(pScreen)
+
+# Construct a one column matrix of the confirmation p-values (transcripts)
+
+pConfirmation <- matrix(res.txp$pvalue, ncol=1)
+rownames(pConfirmation) <- strp(res.txp$feature_id)
+head(pConfirmation)
+
+# Make a data frame with identifiers
+
+tx2gene <- res.txp[,c("feature_id", "gene_id")]
+for (i in 1:2) tx2gene[,i] <- strp(tx2gene[,i])
+head(tx2gene)
+
+# Do analysis
+#Make object for txs
+stageRObj <- stageRTx(pScreen=pScreen, pConfirmation=pConfirmation,
+                      pScreenAdjusted=FALSE, tx2gene=tx2gene)
+
+# Test that object for DTU
+stageRObj <- stageWiseAdjustment(stageRObj, method="dtu", alpha=0.05)
+suppressWarnings({
+  drim.padj <- getAdjustedPValues(stageRObj, order=FALSE,
+                                  onlySignificantGenes=TRUE)
+})
+
+# Table with screened genes
+# Lists genes and transcripts that pass the alpha 0.05
+head(drim.padj)
+
+#### Post Hoc ####
+
+# Filtering out small effect size to correct for false discovery
+
+res.txp.filt <- DRIMSeq::results(d, level="feature")
+smallProportionSD <- function(d, filter=0.1) {
+  cts <- as.matrix(subset(counts(d), select=-c(gene_id, feature_id)))
+  gene.cts <- rowsum(cts, counts(d)$gene_id)
+  total.cts <- gene.cts[match(counts(d)$gene_id, rownames(gene.cts)),]
+  props <- cts/total.cts
+  propSD <- sqrt(rowVars(props))
+  propSD < filter
+}
+filt <- smallProportionSD(d)
+res.txp.filt$pvalue[filt] <- 1
+res.txp.filt$adj_pvalue[filt] <- 1
+res.txp
+res.txp[res.txp$adj_pvalue < 0.05,]
